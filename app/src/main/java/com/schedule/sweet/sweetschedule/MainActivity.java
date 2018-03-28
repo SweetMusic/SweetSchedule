f99e6eb592176772a0c7d151358cdd642b8491ad
SweetMusic <SweetMusic2016@163.com>
2018-03-28 23:19:35 +0800

    test fragment

--- app/src/main/java/com/schedule/sweet/sweetschedule/MainActivity.java
+++ app/src/main/java/com/schedule/sweet/sweetschedule/MainActivity.java
@@ -1,13 +1,28 @@
 package com.schedule.sweet.sweetschedule;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.schedule.sweet.sweetschedule.fragment.Schedule;
+import com.schedule.sweet.sweetschedule.fragment.TestFragment;
+
+public class MainActivity extends AppCompatActivity implements Schedule.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Schedule schedule = Schedule.newInstance("123", "456");
+        getFragmentManager().beginTransaction().add(R.id.fl_container, schedule).commitAllowingStateLoss();
+        //TestFragment tf = new TestFragment();
+        //getFragmentManager().beginTransaction().add(R.id.fl_container, tf).commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/schedule/sweet/sweetschedule/fragment/TestFragment.java
@@ -0,0 +1,33 @@
+package com.schedule.sweet.sweetschedule.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.schedule.sweet.sweetschedule.R;
+
+/**
+ * Created by sweet on 2018/3/28.
+ */
+
+public class TestFragment extends Fragment {
+
+    private TextView mTv;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.test_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTv = view.findViewById(R.id.test_tv);
+    }
+}
